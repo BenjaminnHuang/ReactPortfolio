5fc276038c62a3bab5e81dadfb9e76d03d05b3e4
BenjaminnHuang <peternick0207@gmail.com>
2023-04-18 23:38:06 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Intro from "./components/Intro"
 import Footer from "./components/Footer"
 import Experience from "./components/Experience"
 import Contact from "./components/Contact"
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
