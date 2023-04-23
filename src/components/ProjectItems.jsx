cc1fbf0e74a33057734dcfc83f33c72d87eef3c9
BenjaminnHuang <peternick0207@gmail.com>
2023-04-23 03:10:14 -0700

    first commit

--- src/components/ProjectItems.jsx
+++ src/components/ProjectItems.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectItems({link, title, description, image}){
     return(
 
-        <div className="flex flex-col md:flex-row m-16 w-full bg-slate-700 rounded-lg overflow-hidden max-w-6xl">
+        <div className="flex flex-col md:flex-row m-8 w-full bg-slate-700 rounded-lg overflow-hidden max-w-6xl">
             <img src={image} alt="project" className="m-6 h-54 md:h-96 w-fit cursor-pointer object-cover "/>
             <div className=" my-6 mx-20 flex flex-col text-white w-1/2">
                 <div className="text-6xl mt-6 text-center">{title}</div>
